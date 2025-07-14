93077c1acfcf74c1143c390daee103b9630a82a9
alexbuturlakin <alexbuturlakin@gmail.com>
2025-07-14 23:35:19 +0300

    Make AbstractCallbackAnnotation repeatable

--- src/Annotations/AbstractCallbackAnnotation.php
+++ src/Annotations/AbstractCallbackAnnotation.php
@@ -10,11 +10,11 @@ use TinyRest\Attributes\AttributeTrait;
  * @Annotation
  * @Target({"CLASS"})
  */
-#[Attribute(Attribute::TARGET_CLASS)]
+#[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
 abstract class AbstractCallbackAnnotation implements AttributeInterface
 {
     use AttributeTrait;
-    
+
     public function __construct(array $options = [], public ?string $method = null, public ?string $callback = null)
     {
         $this->populateProperties($options);
